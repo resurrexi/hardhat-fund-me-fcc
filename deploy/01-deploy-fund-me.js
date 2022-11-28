a6ce82c674c2118958c0bd7370450443649dd7fc
Liquan Yang <yang.liquan87@gmail.com>
2022-11-27 21:32:49 -0500

    finish scripts and add mock contract

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -1,15 +1,32 @@
 const { networkConfig } = require("../helper-hardhat-config");
+const { verify } = require("../utils/verify");
 
 module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
-  const { deploy, log } = deployments;
+  const { deploy, get } = deployments;
   const { deployer } = await getNamedAccounts();
   const chainId = await getChainId();
 
-  const ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"];
+  let ethUsdPriceFeedAddress;
+
+  // 31337 is localhost
+  if (chainId == "31337") {
+    const ethUsdAggregator = await get("MockV3Aggregator");
+    ethUsdPriceFeedAddress = ethUsdAggregator.address;
+  } else {
+    ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"];
+  }
 
   const fundMe = await deploy("FundMe", {
     from: deployer,
     args: [ethUsdPriceFeedAddress], // put priceFeed address (see FundMe constructor)
     log: true,
+    waitConfirmations: chainId != "31337" ? 6 : 1, // wait 6 block confirmations if network isn't localhost
   });
+
+  if (chainId != "31337" && process.env.ETHERSCAN_API_KEY) {
+    // verify after deployment if network isn't localhost
+    await verify(fundMe.address, [ethUsdPriceFeedAddress]);
+  }
 };
+
+module.exports.tags = ["all", "fundme"];
